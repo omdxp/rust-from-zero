c1223ca8fc73fbe2c09f6a3931db062a817a4e6b
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-09-04 10:28:51 +0100

    app entrypoint

---
+++ 02.app_entrypoint/src/main.rs
@@ -0,0 +1,10 @@
+#![allow(dead_code)] // ex: unused variables
+#![allow(unused_imports)]
+
+fn main() {
+    println!("test");
+}
+
+// main function is invoked when the program starts where and as a result you cannot have several main functions
+// println! is a macro which to outputs to the console
+// the first 2 instructions turn off some of the warning that the rust compiler gives you
